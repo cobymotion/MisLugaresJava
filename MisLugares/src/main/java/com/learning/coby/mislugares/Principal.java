eb24f883ab0a22335e052dc4c4ab8070b53bf25e
luiscobian <coby10@gmail.com>
2018-04-29 18:17:18 -0500

    Initial commit Curso de android Jose Tomas

---
+++ MisLugares/src/main/java/com/learning/coby/mislugares/Principal.java
@@ -0,0 +1,11 @@
+package com.learning.coby.mislugares;
+
+public class Principal {
+    public static void main(String[] args) {
+        Lugar lugar = new Lugar("Escuela Politécnica Superior de Gandía",
+                "C/ Paranimf, 1 46730 Gandia (SPAIN)", -0.166093, 38.995656,
+                962849300, "http://www.epsg.upv.es",
+                "Uno de los mejores lugares para formarse.", 3);
+        System.out.println("Lugar " + lugar.toString());
+    }
+}
